bbb559aea26d15c6ee3f6074a27578ee7511e18e
Anupama Saranu <anupama.saranu@cognine.com>
2024-02-02 16:12:41 +0530

    Removed Empty Lines

--- pythonProject/IntermediateLevel/Lambda Function.py
+++ pythonProject/IntermediateLevel/Lambda Function.py
@@ -106,14 +106,3 @@ import functools
 lis = [1, 3, 5, 6, 2, ]
 print("The maximum element of the list is : ", end="")
 print(functools.reduce(lambda a, b: a if a > b else b, lis))    # Output is The maximum element of the list is : 6
-
-
-
-
-
-
-
-
-
-
-
