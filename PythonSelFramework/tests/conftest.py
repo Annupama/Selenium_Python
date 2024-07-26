6895348e3021293d6a76363bddcaa08232c080c0
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2024-07-26 16:35:28 +0530

    Add files via upload
    Add files

---
+++ PythonSelFramework/tests/conftest.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.support import expected_conditions as EC
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture(scope="class")
+#testfine
+def setup(request):
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
+    driver.get("https://chorusqa.cogninelabs.com/")
+    driver.maximize_window()
+    driver.find_element(By.XPATH, "//input[@type='email']").send_keys("chorus.automation@cognine.com")
+    driver.find_element(By.XPATH, "//input[@id='idSIButton9']").click()
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@type='password']")))
+    driver.find_element(By.XPATH, "//input[@type='password']").send_keys("Welcome2Cognine")
+    driver.find_element(By.XPATH, "//input[@id='idSIButton9']").click()
+    driver.implicitly_wait(5)
+    driver.find_element(By.XPATH, "//input[@id='idSIButton9']").click()
+    driver.implicitly_wait(20)
+    request.cls.driver = driver
+    yield
+    driver.close()
