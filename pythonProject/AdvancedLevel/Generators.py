07ea18048e63a7794f73bcf451a15e8d39eca8e5
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2024-08-28 16:30:57 +0530

    Updated with few more examples

--- pythonProject/AdvancedLevel/Generators.py
+++ pythonProject/AdvancedLevel/Generators.py
@@ -12,7 +12,7 @@ gen = simple_generator()
 print(next(gen))  # Output: 1
 print(next(gen))  # Output: 2
 print(next(gen))  # Output: 3
-print(next(gen))  # Raises StopIteration
+# print(next(gen))  # Raises StopIteration
 
 # Using Generators in loop
 
@@ -115,5 +115,62 @@ even_gen = filter_even(numbers)
 
 for even in even_gen:
     print(even)         # Output is 2, 4, 6
+
+# Prime Number Generator
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+        return True
+
+
+def prime_generator():
+    n = 2
+    while True:
+        if is_prime(n):
+            yield n
+        n += 1
+
+
+primes = prime_generator()
+for _ in range(10):  # Print the first 10 prime numbers
+    print(next(primes))      # Output is 2 3 5 7 11 13 17 19 23 29
+
+
+# Generator for Chunking a List
+
+def chunk_list(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+chunks = chunk_list(data, 3)
+
+for chunk in chunks:
+    print(chunk)   # Output is [1, 2, 3], [4, 5, 6], [7, 8, 9]
+
+# Processing data in stages.
+
+def stage_one(data):
+    for item in data:
+        if item % 2 == 0:
+            yield item
+
+def stage_two(data):
+    for item in data:
+        yield item * 2
+
+data = range(10)
+pipeline = stage_two(stage_one(data))
+
+for item in pipeline:
+    print(item)
+
+
 
 
