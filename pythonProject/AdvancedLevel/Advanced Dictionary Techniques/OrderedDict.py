4ab4565dbe28f238d50451b7245306eac2e586ba
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2024-08-30 17:25:23 +0530

    Update OrderedDict.py

--- pythonProject/AdvancedLevel/Advanced Dictionary Techniques/OrderedDict.py
+++ pythonProject/AdvancedLevel/Advanced Dictionary Techniques/OrderedDict.py
@@ -4,6 +4,6 @@ ordered = OrderedDict()
 ordered['apple'] = 3
 ordered['Banana'] = 2
 ordered['Cherry'] = 5
-print(ordered)                 # Output: OrderedDict([('apple', 3), ('banana', 2), ('cherry', 5)])
-ordered.move_to_end('Banana')  # Moves 'banana' to the end
+print(ordered)                  # Output: OrderedDict([('apple', 3), ('banana', 2), ('cherry', 5)])
+ordered.move_to_end('Banana')   # Moves 'banana' to the end
 print(ordered)                  # Output: OrderedDict([('apple', 3), ('cherry', 5), ('banana', 2)])
